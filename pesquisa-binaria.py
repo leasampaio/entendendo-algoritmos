32a8addb44490a8419df7b0cad3d5402fd5d807b
Lea Sampaio <lealisboas@gmail.com>
2021-08-10 20:10:31 -0300

    feat: atividade 1

---
+++ pesquisa-binaria.py
@@ -0,0 +1,17 @@
+def pesquisa (lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = int((baixo+alto)/2)
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+minha_lista = [1, 3, 5, 7, 9]
+
+print(pesquisa(minha_lista, 3))
+print(pesquisa(minha_lista, -1))
